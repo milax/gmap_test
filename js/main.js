6adbe1ac056c7ab62216d34198fe8d0aad883389
Eugene Kuzmin <eugene.kuzmin@creuna.com>
2015-02-18 15:57:15 +0200

    Refactoring

--- js/main.js
+++ js/main.js
@@ -26,11 +26,11 @@ app.controller('gmapController', function($scope, $q) {
       center: {
         lat: 50.05,
         lng: 36.23,
-        address: ''
       },
       zoom: 6
   };
 
+  // center types
   var center_types = [{
       name: 'Sprogcenter',
       marker_url: 'img/marker-pink.png',
@@ -45,34 +45,63 @@ app.controller('gmapController', function($scope, $q) {
       active_marker_url: 'img/marker-green-active.png'
     }];
 
+  // active center type by default
   var ACTIVE_CENTER_TYPE_INDEX = 0;
 
   $scope.center_types                = center_types;
-  $scope.number_of_centers           = 1;
-  $scope.selected_center_type        = ACTIVE_CENTER_TYPE_INDEX;
+  $scope.number_of_centers           = 0;
+
+  $scope.active_center_type_index    = ACTIVE_CENTER_TYPE_INDEX;
   $scope.active_center_type          = center_types[ACTIVE_CENTER_TYPE_INDEX];
-  $scope.filter_selected_center_type = ACTIVE_CENTER_TYPE_INDEX;
-  $scope.filter_center_type          = center_types[ACTIVE_CENTER_TYPE_INDEX];
+  // $scope.filter_selected_center_type = ACTIVE_CENTER_TYPE_INDEX;
+  // $scope.filter_center_type          = center_types[ACTIVE_CENTER_TYPE_INDEX];
 
-  $scope.lat = '';
-  $scope.lng = '';
+  $scope.lat     = '';
+  $scope.lng     = '';
+  $scope.address = '';
 
-  var addMarker = function(e) {
+  var info_panel = {
+    updateWithCoords: function(coords) {
+      $scope.lat = coords.lat;
+      $scope.lng = coords.lng;
+      $scope.$apply();
+    },
+    clearData: function() {
+      $scope.address = '';
+      $scope.lat     = '';
+      $scope.lng     = '';
+    }
+  };
+
+  /**
+  * Add new marker by click on the map
+  */
+  var addNewMarkerByClick = function(e) {
+    addNewMarker({
+      lat: e.latLng.k,
+      lng: e.latLng.D
+    });
+  };
+
+  /**
+  * Add new marker with coords
+  */
+  var addNewMarker = function(coords) {
     var marker = new gmaps.Marker({
         map: map,
-        position: e.latLng,
+        position: coords,
         icon: $scope.active_center_type.marker_url,
         draggable: true
     });
 
     gmaps.event.addListener(marker, "dragend", function(e) {
-      setThisMarkerActive(marker);
-      setMarkerNewPos(e.latLng);
+      setMarkerActive(marker);
+      updateInfoPanel(coords);
       getAddress();
     });
 
     gmaps.event.addListener(marker, "click", function(e) {
-      setThisMarkerActive(marker);
+      setMarkerActive(marker);
     });
 
     gmaps.event.addListener(marker, "dblclick", function (e) {
@@ -84,15 +113,20 @@ app.controller('gmapController', function($scope, $q) {
     marker_index++;
     markers.push(marker);
 
-    setThisMarkerActive(marker);
+    setMarkerActive(marker);
 
-    $scope.lat = e.latLng.k;
-    $scope.lng = e.latLng.D;
-    $scope.$apply();
+    info_panel.updateWithCoords({
+      lat: coords.lat,
+      lng: coords.lng
+    });
+
     getAddress();
   };
 
-  var setThisMarkerActive = function(marker) {
+  /**
+  * Set the marker active
+  */
+  var setMarkerActive = function(marker) {
     if(active_marker) {
       active_marker.setIcon(active_marker.center_type.marker_url);
     }
@@ -100,29 +134,27 @@ app.controller('gmapController', function($scope, $q) {
     active_marker.setIcon(active_marker.center_type.active_marker_url);
 
     getAddress();
-    updateCoordsWithActiveMarker();
+    info_panel.updateWithCoords({
+      lat: active_marker.position.k,
+      lng: active_marker.position.D
+    });
     updateCenterType();
   };
 
-  var setMarkerNewPos = function(location) {
-    $scope.lat = location.k;
-    $scope.lng = location.D;
-    $scope.$apply();
-  };
-
-  var updateCoordsWithActiveMarker = function() {
-    $scope.lat = active_marker.position.k;
-    $scope.lng = active_marker.position.D;
-    $scope.$apply();
-  };
-
+  /**
+  * Update center type
+  */
   var updateCenterType = function() {
     $scope.active_center_type = active_marker.center_type;
     $scope.$apply();
   };
 
-  var placesChanged = function() {
-    var places = searchBox.getPlaces(), place;
+  /**
+  * Address input has been changed
+  */
+  var placesChanged = function(e) {
+    var places = searchBox.getPlaces(),
+      place;
 
     if(!places.length) {
       return;
@@ -130,104 +162,125 @@ app.controller('gmapController', function($scope, $q) {
 
     place = places[0];
 
+    addNewMarker({
+      lat: place.geometry.location.k,
+      lng: place.geometry.location.D
+    });
+
     map.setCenter(place.geometry.location);
     map.setZoom(14);
-
-    // $scope.lat = place.geometry.location.k;
-    // $scope.lng = place.geometry.location.D;
-    // $scope.$apply();
-    // getAddress();
   };
 
+  /**
+  * Get address for active marker
+  */
   var getAddress = function () {
+    var address = "";
+
     if(!active_marker) {
       return;
     }
+
     geocoder.geocode({
       location: active_marker.getPosition()
     }, function (result, status) {
-      var address = "";
-
       if (status == gmaps.GeocoderStatus.OK) {
         address = result[0].formatted_address;
         $scope.address = address;
-
         $scope.$apply();
       }
     });
   };
 
-  var clearMarkerDataInPanel = function() {
-    $scope.address = '';
-    $scope.lat = '';
-    $scope.lng = '';
-  };
-
+  /**
+  * init map
+  */
   var init = function() {
-    gmaps = google.maps;
-    map = new gmaps.Map(document.getElementById('map'), mapOptions);
+    var input;
+
+    gmaps    = google.maps;
+    map      = new gmaps.Map(document.getElementById('map'), mapOptions);
     geocoder = new gmaps.Geocoder();
 
-    var input = document.getElementById('search-val');
-    searchBox = new gmaps.places.SearchBox(input);
+    address_input = document.getElementById('search-val');
+    searchBox     = new gmaps.places.SearchBox(address_input);
 
-    gmaps.event.addListener(map, "click", addMarker);
+    // attach events
+    gmaps.event.addListener(map, "click", addNewMarkerByClick);
     gmaps.event.addListener(searchBox, 'places_changed', placesChanged);
-
-    getAddress();
-
   };
 
-  $scope.removeAllCenters = function() {
+  /**
+  * Remove all markers from the map
+  */
+  $scope.removeAllMarkers = function() {
     for(var i = 0, len = markers.length; i < len; ++i) {
       markers[i].setMap(null);
     }
-    clearMarkerDataInPanel();
+    info_panel.clearData();
     markers = [];
     return false;
   };
 
-  $scope.removeCurrentCenter = function() {
+  /**
+  * Remove active marker from the map
+  */
+  $scope.removeActiveMarker = function() {
     for(var i = 0, len = markers.length; i < len; ++i) {
       if(markers[i].marker_index === active_marker.marker_index) {
         active_marker.setMap(null);
         markers.splice(i, 1);
-        clearMarkerDataInPanel();
+        info_panel.clearData();
         break;
       }
     }
     return false;
   };
 
-  $scope.isAnyMarker = function() {
+  /**
+  * Is any marker exists?
+  */
+  $scope.isAnyMarkerExists = function() {
     return markers.length;
   };
 
-  $scope.filterTypeSelected = function() {
-    $scope.filter_center_type = center_types[$scope.filter_selected_center_type];
-  };
+  // $scope.filterTypeSelected = function() {
+  //   $scope.filter_center_type = center_types[$scope.filter_selected_center_type];
+  // };
 
-  $scope.typeSelected = function() {
-    $scope.active_center_type = center_types[$scope.selected_center_type];
+  /**
+  * Center type has been selected in selector
+  */
+  $scope.centerTypeSelected = function() {
+    $scope.active_center_type = center_types[$scope.active_center_type_index];
     active_marker.center_type = $scope.active_center_type;
     active_marker.setIcon(active_marker.center_type.active_marker_url);
   };
 
+  /*
+  * ng-repeat of element has finished its work
+  */
   $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent, element) {
-    function formatOption (opt) {
-      if(!opt.id) {
-        return '';
-      }
-      var option = ('<span style="white-space: nowrap"><img src="' + center_types[opt.element.value].marker_url + '" />' + opt.text + '</span>');
-      return option;
-    }
-
-    $(element).parent().select2({
+    var $select = $(element).parent();
+    $select.select2({
       templateResult: formatOption,
       minimumResultsForSearch: 100
     });
+
+    function formatOption (opt) {
+      var option;
+
+      if(!opt.id) {
+        return '';
+      }
+      option = '<span style="white-space: nowrap"><img src="' + center_types[opt.element.value].marker_url + '" />' + opt.text + '</span>';
+      return option;
+    }
   });
 
+  /**
+  * Run `init` function on window load event
+  */
   google.maps.event.addDomListener(window, 'load', init);
 
 });
